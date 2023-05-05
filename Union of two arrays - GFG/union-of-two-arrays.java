1949dc063a6a3d98d08675535ec88f07d98342df
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2023-05-05 16:53:21 +0530

    Added solution - LeetHub

---
+++ Union of two arrays - GFG/union-of-two-arrays.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //Taking input using class Scanner
+		Scanner sc=new Scanner(System.in);
+		
+		//Taking total count of testcases
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    
+		    int n,m;
+		    
+		    //taking size of array a
+		    n=sc.nextInt();
+		    
+		    //taking size of array b
+		    m=sc.nextInt();
+		    
+		    //Creating 2 array of size n and m
+		    int a[]=new int[n];
+		    int b[]=new int[m];
+		    
+		    //inserting elements to array a
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextInt();
+		    }
+		    
+		    //inserting elements to array b
+		    for(int i=0;i<m;i++)
+		    {
+		        b[i]=sc.nextInt();
+		    }
+		    Solution ob=new Solution();
+		    //calling doUnion method and printing the results
+		    System.out.println(ob.doUnion(a,n,b,m));
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        //Your code here
+        Set<Integer> unionSet = new HashSet<>();
+
+        // Traverse the first array and add each element to the hash set
+        for (int i = 0; i < n; i++) {
+            unionSet.add(a[i]);
+        }
+
+        // Traverse the second array and add each element to the hash set
+        for (int i = 0; i < m; i++) {
+            unionSet.add(b[i]);
+        }
+
+        // The size of the hash set will give us the number of elements in the union of the two arrays
+        return unionSet.size();
+    }
+        
+    
+}
