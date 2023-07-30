ebf35143bdd7f36a9fcd7b68b2827a73dc4e8990
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2023-07-30 20:26:25 +0530

    Added solution - LeetHub

---
+++ Check for Binary - GFG/check-for-binary.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class checkBinary
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		sc.nextLine();
+		while(T>0)
+		{
+			String str = sc.nextLine();
+			GfG g = new GfG();
+			boolean b = g.isBinary(str);
+			if(b== true)
+				System.out.println(1);
+			else
+			    System.out.println(0);
+		T--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class GfG
+{
+	boolean isBinary(String str)
+	{
+	  //Your code here
+	  for (char c : str.toCharArray()) {
+    if (c != '0' && c != '1') {
+      return false;
+    }
+  }
+  return true;
+	}
+}
