d065f46257b1759a6f61e8d78a3b37812684f6ae
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2023-09-05 00:58:46 +0530

    Time: 2 ms (85.14%), Space: 40.3 MB (78.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(' ||
+                       s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{' ||
+                       s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
