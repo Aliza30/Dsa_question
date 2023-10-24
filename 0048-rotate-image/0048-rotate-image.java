661299be3c242ebbfd1ac671d8c49ca0feb45871
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2023-10-24 11:30:48 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (18.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,35 +1,20 @@
 class Solution {
     public void rotate(int[][] matrix) {
-         int size = matrix.length;
-
-        for (int x0 = 0, x1 = size - 1; x0 <= x1; x0++, x1--) {
-            for (int y0 = 0, y1 = size - 1; y0 < y1; y0++, y1--) {
-                swap(matrix, x0, y0, x1, y1);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
     }
-
-    private static void swap(int[][] matrix, int x0, int y0, int x1, int y1) {
-        // x 1 x x      x0 = 1, x1 = 2
-        // x x x 2      y0 = 0, y1 = 3 
-        // 4 x x x 
-        // x x 3 x 
-
-        // keep 1
-        int t = matrix[y0][x0];
-
-        // 4 -> 1
-        matrix[y0][x0] = matrix[x1][y0];
-
-        // 3 -> 4
-        matrix[x1][y0] = matrix[y1][x1];
-
-        // 2 -> 3
-        matrix[y1][x1] = matrix[x0][y1];
-
-        // 1 -> 2
-        matrix[x0][y1] = t;
-    
-    }
+}
 }
