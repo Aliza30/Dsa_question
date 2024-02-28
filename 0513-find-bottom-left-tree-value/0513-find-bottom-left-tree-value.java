4392efdee04b25117586fdfdd1aba31f15c44a64
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2024-02-28 14:34:46 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (98.61%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     public int findBottomLeftValue(TreeNode root) {
+        return findBottomLeftValue(root, 1, new int[]{0,0});
+    }
+    public int findBottomLeftValue(TreeNode root, int depth, int[] res) {
+        if (res[1]<depth) {res[0]=root.val;res[1]=depth;}
+        if (root.left!=null) findBottomLeftValue(root.left, depth+1, res);
+        if (root.right!=null) findBottomLeftValue(root.right, depth+1, res);
+        return res[0];
+    }
+}
