27f8e1936e5833df4fb781e2e151e91548819a82
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2024-08-13 12:41:14 +0530

    Time: 1 ms (71.22%), Space: 42.1 MB (43.62%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,20 +1,16 @@
 class Solution {
-    public int gcd(int x, int y) {
-        if (y == 0) {
-            return x;
-        } else {
-            return gcd(y, x % y);
-        }    
-    }
-    
     public String gcdOfStrings(String str1, String str2) {
-        // Check if they have non-zero GCD string.
-        if (!(str1 + str2).equals(str2 + str1)) {
+         if (!(str1 + str2).equals(str2 + str1)) {
             return "";
         }
-        
-        // Get the GCD of the two lengths.
-        int gcdLength = gcd(str1.length(), str2.length());
-        return str1.substring(0, gcdLength);
+        // int gcdLength = gcd(str1.length(), str2.length());
+        // return str1.substring(0, gcdLength);
+        int gcdL=gcd(str1.length(),str2.length());
+            return str1.substring(0,gcdL);
+    }
+    public int gcd(int x, int y){
+        if(y==0) return x;
+        else 
+            return gcd(y,x%y);
     }
 }
