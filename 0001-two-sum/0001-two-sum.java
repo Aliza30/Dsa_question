38d095b1215148e8ae7f12090dea667c65f143d0
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2024-11-14 11:54:10 +0530

    Time: 3 ms (64.49%), Space: 44.8 MB (48.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,19 @@
-class Solution
-{
-    public int[] twoSum(int[] nums, int target) 
-    {
-         int[] resultArray = new int[2];
+class Solution {
 
-        // Initialize hashmap to store previously checked values.
-        // Key of map will store value in array and value of map will store index in array.
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            // (target - nums[i]) exits in map then store indices in results. That's the answer.
-            if (map.get(target - nums[i]) != null) {
-                resultArray[0] = i;
-                resultArray[1] = map.get(target - nums[i]);
-                break;
-            } else map.put(nums[i] , i);
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
+            }
+
+            prevMap.put(num, i);
         }
-        return resultArray;
+
+        return new int[] {};
     }
 }
