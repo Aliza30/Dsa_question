d86e4e5c1ba81f846b4e2f7a106e36d50dc1a382
ALIZA RAZI <104764771+Aliza30@users.noreply.github.com>
2025-04-11 12:23:37 +0530

    Time: 42 ms (69.19%) | Memory: 53.8 MB (46.72%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -3,23 +3,14 @@
  * @param {string} word2
  * @return {string}
  */
-// var mergeAlternately = function(word1, word2) {
-//   const result='';
-//   const max = Math.max(word1.length, word2.length);
-// for( let i in max){
-// if (i < word1.length) result += word1[i];
-//     if (i < word2.length) result += word2[i];
-// }
-// return result;
-// };
 var mergeAlternately = function(word1, word2) {
-  let result = '';
-  const max = Math.max(word1.length, word2.length);
-  
-  for (let i = 0; i < max; i++) {
-    if (i < word1.length) result += word1[i];
-    if (i < word2.length) result += word2[i];
-  }
+    let merged = '';
+    const maxLength = Math.max(word1.length, word2.length);
 
-  return result;
+    for (let i = 0; i < maxLength; i++) {
+        if (i < word1.length) merged += word1[i];
+        if (i < word2.length) merged += word2[i];
+    }
+
+    return merged;
 };
